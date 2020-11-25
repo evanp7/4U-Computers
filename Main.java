57aa5dbc1eae543b422b69a74a500f40870e988e
evanp7 <parkevan169@gotvdsb.ca>
2020-11-25 17:26:24 +0000

    Working on question 1

--- Main.java
+++ Main.java
@@ -7,25 +7,24 @@ class Main {
     Scanner input = new Scanner(System.in);
 
 // Variables
-    String ProNoun;
-    String Noun;
-    String Verb;
-    String Sentence;
+Boolean Answer1;
 
-//Input Screen
-    System.out.println("Please enter a proper noun: ");
-    ProNoun = input.nextLine();
 
-    System.out.println("Please enter a noun: ");
-    Noun = input.nextLine();
+// Question 1
+    System.out.println("This is a true or false question, please type true or false");
+    System.out.println("Tesla is expected to grow in 2020");
+    Answer1 = input.nextBoolean();
+    if 
 
-    System.out.println("Please enter a verb: ");
-    Verb = input.nextLine();
 
-// Replace integers w/ strings *(treat 1, 2 & 3 as "text" instead of integers to use String.replaceAll)
-    Sentence = "1 was an interesting 2, but a unique 2. They could 3 better than any other 2. 3ing is 1’s favorite activity.";
 
-    System.out.print(Sentence.replaceAll("1", ProNoun).replaceAll("2", Noun).replaceAll("3", Verb));
+
+
+
+
+
+
+
 
 
   }
