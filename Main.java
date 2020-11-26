9d0ac55f8f0a5265f4ae1735d438672789607ae6
evanp7 <parkevan169@gotvdsb.ca>
2020-11-26 19:34:10 +0000

    Working on question 3 & 4 for 1.5

--- Main.java
+++ Main.java
@@ -6,13 +6,13 @@ class Main {
 // Scanner 
     Scanner input = new Scanner(System.in);
 
-// Variables for Question 1
-Boolean Answer1;
+//Question 1
+Boolean Answer1; // Variable
 
-// Question 1
     System.out.println("This is a true or false question, please type 'True' or 'False'");
     System.out.println("Does Tesla make electric cars?");
     Answer1 = input.nextBoolean();
+
     if (Answer1 == true)
     {
       System.out.println("That is correct.");
@@ -28,42 +28,32 @@ Boolean Answer1;
     }
 
 
-// Variables for Question 2
-char Answer2 = "C";
-
 // Question 2
+String Answer2; // Variable
+
     System.out.println("This is a multiple choice question");
     System.out.println("What is 2^5? Please type A, B, C or D");
     System.out.println("A) 10");
     System.out.println("B) 30");
     System.out.println("C) 32");
     System.out.println("D) 7");
-    if (Answer2 == A)
-    {
-      System.out.println("That is incorrect.");
-    }
-    if (Answer2 == "B") 
-    {
-      System.out.pritln("That is incorrect.");
-    }
+    Answer2 = input.nextLine();
+
     if (Answer2 == "C")
     {
       System.out.println("That is correct.");
     }
-    if (Answer2 == "D")
-    {
-      System.out.println("That is incorrect");
-    }
     else
     {
-      System.out.println("Please try again.");
+      System.out.println("That is incorrect. Please try again.");
     }
 
 
+// Question 3
+int Answer3;
 
-
-
-
+    System.out.println("What years are in 21st century?");
+    
 
 
 
